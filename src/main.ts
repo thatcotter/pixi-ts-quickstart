5dfdf238abb08c57176f023b3c126dc19be1a90a
Andrew Cotter <cotter.andrew.p@gmail.com>
2022-02-06 20:23:51 -0500

    shaders/filters demo

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import * as PIXI from "pixi.js"
-// import * as Filters from 'pixi-filters'
+import * as Filters from 'pixi-filters'
 
 const load = (app: PIXI.Application) => {
     return new Promise<void>((resolve) => {
         app.loader
             .add('world', 'assets/hello-world.png')
-            // .add('shader', 'assets/shader.frag')
+            .add('shader', 'assets/shader.frag')
             .load(() => {
             resolve();
         });
@@ -14,7 +14,7 @@ const load = (app: PIXI.Application) => {
 
 const main = async () => {
     // Actual app
-    let app = new PIXI.Application();
+    let app = new PIXI.Application({antialias: true});
 
     // Display application properly
     document.body.style.margin = '0';
@@ -26,25 +26,59 @@ const main = async () => {
 
     // Load assets
     await load(app);
+
+    // let container = new PIXI.Container();
+    // container.x = window.innerWidth / 2;
+    // container.y = window.innerHeight / 2;
+    // app.stage.addChild(container);
+
+
     let sprite = new PIXI.Sprite(
         app.loader.resources.world.texture
     );
-    sprite.x = window.innerWidth / 2 - sprite.width / 2;
-    sprite.y = window.innerHeight / 2 - sprite.height / 2;
+    sprite.anchor.set(0.5, 0.5)
+    sprite.scale.set(0.5,0.5)
+    sprite.x = window.innerWidth / 2;
+    sprite.y = window.innerHeight / 2;
+    let myFilter = new PIXI.Filter(undefined, app.loader.resources.shader.data, {time: 0});
+    sprite.filters = [myFilter]
     app.stage.addChild(sprite);
+
+    // let sprite2 = new PIXI.Sprite( app.loader.resources.world.texture);
+    // sprite2.anchor.set(0.5, 0.5)
+    // sprite2.scale.set(0.5,0.5)
+    // sprite2.x = 200
+    // container.addChild(sprite2)
+
+    // let sprite3 = new PIXI.Sprite( app.loader.resources.world.texture);
+    // sprite3.anchor.set(0.5, 0.5)
+    // sprite3.scale.set(0.5,0.5)
+    // sprite3.y = 200
+    // container.addChild(sprite3)
+
+    // let sprite4 = new PIXI.Sprite( app.loader.resources.world.texture);
+    // sprite4.anchor.set(0.5, 0.5)
+    // sprite4.scale.set(0.5,0.5)
+    // sprite4.y = -200
+    // container.addChild(sprite4)
 
     // Handle window resizing
     window.addEventListener('resize', (_e) => {
         app.renderer.resize(window.innerWidth, window.innerHeight);
         sprite.x = window.innerWidth / 2 - sprite.width / 2;
         sprite.y = window.innerHeight / 2 - sprite.height / 2;
+        // container.x = window.innerWidth / 2
+        // container.y = window.innerHeight / 2
     });
 
     document.body.appendChild(app.view);
 
+    let time = 0
+
     let context = {
         velocity: { x: 1, y: 1 },
-        sprite
+        sprite,
+        time
     };
 
     app.ticker.add(update, context);
@@ -52,14 +86,29 @@ const main = async () => {
 
 // Cannot be an arrow function. Arrow functions cannot have a 'this' parameter.
 function update(this: any, delta: number) {
-    if (this.sprite.x <= 0 || this.sprite.x >= window.innerWidth - this.sprite.width) {
-        this.velocity.x = -this.velocity.x;
-    }
-    if (this.sprite.y <= 0 || this.sprite.y >= window.innerHeight - this.sprite.height) {
-        this.velocity.y = -this.velocity.y;
-    }
-    this.sprite.x += this.velocity.x * delta;
-    this.sprite.y += this.velocity.y * delta;
+    // if (this.container.x <= 0 || this.container.x >= window.innerWidth - this.container.width) {
+    //     this.velocity.x = -this.velocity.x;
+    // }
+    // if (this.container.y <= 0 || this.container.y >= window.innerHeight - this.container.height) {
+    //     this.velocity.y = -this.velocity.y;
+    // }
+
+    this.time += delta * 0.05;
+
+    this.sprite.filters[0].uniforms.time = this.time;
+
+    // this.sprite.filters[0].angle = Math.sin(this.time) * 10
+
+    // this.sprite.filters[0].blur = (Math.sin(this.time) + 1) / 2 * 15
+
+    // this.container.scale.set(
+    //     (Math.cos(this.time) + 1) / 2,
+    //     (Math.sin(this.time) + 1) / 2
+    // )
+
+    // this.container.angle += delta;
+    // this.container.x += this.velocity.x * delta;
+    // this.container.y += this.velocity.y * delta;
 };
 
 main();
